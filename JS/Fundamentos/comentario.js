f0c44226bb6ce259006e94300edc7d227eae78bb
Felipe Augusto <felipemorenotk@outlook.com>
2023-01-13 22:40:18 -0300

    teste

---
+++ JS/Fundamentos/comentario.js
@@ -0,0 +1,14 @@
+// Comentário de uma linha
+console.log('linha 1')
+
+/*
+comentário de 
+múltiplas linhas
+*/
+console.log('linha 2')
+
+/*
+ * Comentario de
+ * mútiplas linhas
+ */
+console.log('linha 3')
---
+++ JS/Fundamentos/nullUndefined.js
@@ -0,0 +1,23 @@
+// preferir usar null, undefined deixar pra linguagem dizer ou não
+let valor // nao inicializada
+console.log(valor)
+
+valor = null // ausência de valor
+console.log(valor)
+// console.log(valor.toString()) // erro!
+
+const produto = {}
+console.log(produto.preco)
+console.log(produto)
+
+produto.preco = 3.5
+console.log(produto)
+
+produto.preco = undefined // evite atribuir undefined
+console.log(!!produto.preco)
+//delete produto.preco
+console.log(produto)
+
+produto.preco = null // sem preço
+console.log(!!produto.preco)
+console.log(produto)
